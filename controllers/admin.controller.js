0b213ce25c5261e8117e63576acf174815c64f63
Thor Anderson <ta.anderson@gmail.com>
2020-02-10 12:51:32 -0700

    Added mongoose

--- controllers/admin.controller.js
+++ controllers/admin.controller.js
@@ -1,28 +1,29 @@
 import { Product } from '../models/product'
 
 export const postAddProduct = (req, res, next) => {
-    const title = req.body.title
-    const imageUrl = req.body.imageUrl
-    const price = req.body.price
-    const description = req.body.description
-    const product = new Product(title, price, description, imageUrl)
-    product.save()
+  const title = req.body.title
+  const imageUrl = req.body.imageUrl
+  const price = req.body.price
+  const description = req.body.description
+  const product = new Product(title, price, description, imageUrl)
+  product
+    .save()
     .then(result => {
-        console.log('Created Product')
-        res.send('Created Product.  Check your DB')
-        //res.redirect('/admin/products')
+      console.log('Created Product')
+      res.send('Created Product.  Check your DB')
+      //res.redirect('/admin/products')
     })
-    .catch((err) => {
-        console.error(err)
+    .catch(err => {
+      console.error(err)
     })
 }
 
 export const getAllProducts = (req, res, next) => {
-    Product.fetchAll()
+  Product.fetchAll()
     .then(products => {
-        res.json(products)
+      res.json(products)
     })
     .catch(err => {
-        console.log(err)
+      console.log(err)
     })
-} 
+}
